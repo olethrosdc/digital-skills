85537e1b602cf12953f5b292ef8285347f09deee
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2022-12-02 16:53:19 +0100

    Causality more

--- src/causality/stones_data.py
+++ src/causality/stones_data.py
@@ -10,7 +10,7 @@ def generate_data(n_data = 100000, p_large_stones = 0.2, P_A_small = 0.9, P_A_la
     P_cure[1,0] = P_cure_A_large
     P_cure[1,1] = P_cure_B_large
     for t in range(n_data):
-        P_A = P_A_small * (1 - stone_size[t]) + P_A_large * stone_size[t]
+        P_A = 0.2 # P_A_small * (1 - stone_size[t]) + P_A_large * stone_size[t]
         treatment[t] = np.random.choice(2, p = [P_A, 1 - P_A])
         P_cure_t = P_cure[stone_size[t], int(treatment[t])]
         cured[t] = np.random.choice(2, p = [1 - P_cure_t, P_cure_t])
